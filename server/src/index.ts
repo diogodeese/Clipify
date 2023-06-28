9af927ffd0854e95a56c5ee4a5ece53ec4789ee2
Diogo Santos <diogodeese@gmail.com>
2023-06-28 02:06:26 +0100

    feat: server cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,14 @@
+import cors from 'cors'
 import express from 'express'
 import { router } from './routes.js'
 
 const app = express()
 
+app.use(
+  cors({
+    origin: '*',
+  })
+)
 app.use(express.json())
 app.use(router)
 
