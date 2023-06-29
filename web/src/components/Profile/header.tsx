d8054d327617956c53e0253a2287ec58beb661f7
Diogo Santos <diogodeese@gmail.com>
2023-06-29 03:16:07 +0100

    feat: new update profile button

--- web/src/components/Profile/header.tsx
+++ web/src/components/Profile/header.tsx
@@ -1,6 +1,7 @@
 import { ProfileAvatar } from '@components/Avatar/avatar'
 import { Banner } from '@components/banner'
 import { BsThreeDotsVertical } from 'react-icons/bs'
+import { MdOutlineEdit } from 'react-icons/md'
 import { Link } from 'react-router-dom'
 import { User } from 'src/interfaces/user'
 
@@ -24,12 +25,6 @@ export const Header = ({ user }: HeaderProps) => {
               {user.unique_username}
             </span>
           </div>
-          <Link
-            className="w-fit rounded border border-violet-300 bg-violet-200 px-2 py-1 text-xs font-semibold text-violet-600"
-            to={'/account'}
-          >
-            Edit Profile
-          </Link>
         </div>
         <div className="flex h-24 items-center justify-between gap-4">
           <div>132 Posts</div>
@@ -38,6 +33,12 @@ export const Header = ({ user }: HeaderProps) => {
           <hr className="w-8 rotate-90" />
           <div>137 Following</div>
         </div>
+        <Link
+          className="absolute right-12 top-3 inline-flex h-[30px] w-[30px] appearance-none items-center justify-center rounded-full text-neutral-300 transition-colors hover:bg-neutral-600 focus:outline-none"
+          to={'/account'}
+        >
+          <MdOutlineEdit />
+        </Link>
         <button className="absolute right-3 top-3 inline-flex h-[30px] w-[30px] appearance-none items-center justify-center rounded-full text-neutral-300 transition-colors hover:bg-neutral-600 focus:outline-none">
           <BsThreeDotsVertical />
         </button>
