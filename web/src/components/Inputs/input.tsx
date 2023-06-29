c719dd548f6cb47cb04ffd1f313a12ccaeacaa5c
Diogo Santos <diogodeese@gmail.com>
2023-06-29 03:15:17 +0100

    chore: input refactor

--- web/src/components/Inputs/input.tsx
+++ web/src/components/Inputs/input.tsx
@@ -1,13 +1,8 @@
-interface InputProps {
-  defaultValue?: string
-  placeholder: string
-}
-
-export const PasswordInput = (props: InputProps) => {
+export const Input = (props: Partial<HTMLInputElement>) => {
   return (
     <input
       className="h-8 w-64 rounded border border-transparent px-2 py-1 text-sm caret-violet-500 transition-colors focus:border-violet-500 focus:outline-none"
-      type="password"
+      type={props.type}
       defaultValue={props.defaultValue}
       placeholder={props.placeholder}
     />
