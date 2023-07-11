626f7449752bc815799f428a58474a53c39c7965
Diogo Santos <diogodeese@gmail.com>
2023-07-12 00:54:36 +0100

    chore: add new middleware

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,13 +1,22 @@
 import { Router } from 'express'
 import userController from './controllers/user/index.js'
+import { authenticateToken } from './middleware/authenticateToken.js'
 
 const router = Router()
 
 router.get('/user/:id', userController.getUser)
 router.post('/signIn', userController.signInUser)
 router.post('/user', userController.createUser)
-router.patch('/user/uniqueUsername', userController.updateUserUniqueUsername)
-router.patch('/user/username', userController.updateUserUsername)
-router.delete('/user/:id', userController.deleteUser)
+router.patch(
+  '/user/uniqueUsername',
+  authenticateToken,
+  userController.updateUserUniqueUsername
+)
+router.patch(
+  '/user/username',
+  authenticateToken,
+  userController.updateUserUsername
+)
+router.delete('/user/:id', authenticateToken, userController.deleteUser)
 
 export { router }
