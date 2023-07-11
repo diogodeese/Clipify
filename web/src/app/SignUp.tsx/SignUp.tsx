ac909c6f5d28e93be2a1158b77d41b0a56a5b3b5
Diogo Santos <diogodeese@gmail.com>
2023-07-12 00:53:26 +0100

    feat: add sign up link

---
+++ web/src/app/SignUp.tsx/SignUp.tsx
@@ -0,0 +1,11 @@
+export const SignUp = () => {
+  return (
+    <>
+      <div className="flex w-screen flex-col items-center justify-center gap-6 py-8">
+        <button className="flex items-center gap-3 rounded border px-4 py-2">
+          Sign in with Google
+        </button>
+      </div>
+    </>
+  )
+}
--- web/src/components/Navbar/NavigationMenu/linksData.tsx
+++ web/src/components/Navbar/NavigationMenu/linksData.tsx
@@ -3,6 +3,7 @@ import { NavigationMenuType } from './navigationMenu.types'
 const linksData: NavigationMenuType[] = [
   { url: '/games', displayName: 'Games' },
   { url: '/signIn', displayName: 'Sign In' },
+  { url: '/signUp', displayName: 'Sign Up' },
 ]
 
 export default linksData
