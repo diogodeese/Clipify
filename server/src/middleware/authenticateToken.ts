9207eb4d79c7d68cb2fb9ef61e4f5d9b8a909311
Diogo Santos <diogodeese@gmail.com>
2023-07-12 00:53:06 +0100

    chore: rename middleware file

---
+++ server/src/middleware/authenticateToken.ts
@@ -0,0 +1,26 @@
+import { User } from '@prisma/client'
+import { NextFunction, Response } from 'express'
+import jwt from 'jsonwebtoken'
+import { CustomRequest } from '../interfaces/customRequest.js'
+
+export const authenticateToken = async (
+  request: CustomRequest,
+  response: Response,
+  next: NextFunction
+) => {
+  const authHeader = request.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+
+  if (token === 'null') {
+    return response.status(401)
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, user: User) => {
+    if (error) {
+      return response.status(403)
+    }
+
+    request.user = user
+    next()
+  })
+}
