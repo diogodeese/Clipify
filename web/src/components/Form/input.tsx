84704763b1ad68e930e26200e0c4a7e908843aec
Diogo Santos <diogodeese@gmail.com>
2023-07-15 05:21:12 +0100

    feat: form component

---
+++ web/src/components/Form/input.tsx
@@ -0,0 +1,19 @@
+import { InputHTMLAttributes } from 'react'
+import { useFormContext } from 'react-hook-form'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string
+}
+
+export function Input(props: InputProps) {
+  const { register } = useFormContext()
+
+  return (
+    <input
+      id={props.name}
+      className="flex-1 rounded border border-zinc-700 bg-neutral-800 px-3 py-2 text-neutral-200 shadow-sm focus:outline-none"
+      {...register(props.name)}
+      {...props}
+    />
+  )
+}
--- web/src/components/errorMessage.tsx
+++
@@ -1,7 +0,0 @@
-interface ErrorMessageProps {
-  message: string | undefined
-}
-
-export const ErrorMessage = (props: ErrorMessageProps) => {
-  return <span className="text-sm text-red-500">{props.message}</span>
-}
