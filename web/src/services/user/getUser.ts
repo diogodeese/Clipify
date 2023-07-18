38f40a2099456f48d784bfddf3d0c96107a73fe3
Diogo Santos <diogodeese@gmail.com>
2023-07-18 20:33:43 +0100

    chore: rename get user function

--- web/src/services/user/getUser.ts
+++ web/src/services/user/getUser.ts
@@ -1,7 +1,9 @@
 import api from '@api/api'
 
-export const getUser = async (id: string) => {
+const getUser = async (id: string) => {
   const response = await api.get(`http://localhost:4003/user/${id}`)
 
   return response.data
 }
+
+export default getUser
