b54444d8224b9b2b631e049c2421e67e62f1bba1
Diogo Santos <diogodeese@gmail.com>
2023-07-18 20:34:14 +0100

    chore: api post generic function

--- web/src/api/api.ts
+++ web/src/api/api.ts
@@ -7,8 +7,13 @@ const get = async (route: string) => {
   return await axios.get(route)
 }
 
+const post = async <T>(route: string, body: T) => {
+  return await axios.post(route, body)
+}
+
 const api = {
   get,
+  post,
 }
 
 export default api
