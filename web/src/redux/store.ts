e24864a12c4661e24d8141a8a14dad466719d3e0
Diogo Santos <diogodeese@gmail.com>
2023-08-06 23:44:49 +0100

    chore: add persistence to redux storage

--- web/src/redux/store.ts
+++ web/src/redux/store.ts
@@ -1,10 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
-import authReducer from './auth/authSlice'
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import rootReducer from './rootReducer'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
+  reducer: persistedReducer,
 })
 
+export const persistor = persistStore(store)
 export default store
